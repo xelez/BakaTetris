8b82ba90328564e12ad336943d1b01cb32c45e1c
Alex Ankudinov <xelez0@gmail.com>
2015-12-15 15:22:25 +0500

    Implement connection to mongo, signup and signin

--- lobby_server/routes.js
+++ lobby_server/routes.js
@@ -1,14 +1,50 @@
 var express = require('express');
-var router = express.Router();
-
+var MongoClient = require('mongodb').MongoClient
 var HttpError = require('./error').HttpError;
 
-router.post('/signup', function(req, res) {
-    res.send('hello');
+var router = express.Router();
+var db;
+
+MongoClient.connect("mongodb://localhost:27017/bakatetris", function(err, database) {
+    if (err) throw err;
+    db = database;
+    db.collection("users").ensureIndex("user", {unique: true}, function(err, index) {
+        if (err) throw err;
+    });
+})
+
+router.post('/signup', function(req, res, next) {
+    var collection = db.collection("users");
+    var user = req.body.user || "";
+    var passwd = req.body.password || "";
+    if (!user || !passwd)
+        return next(new HttpError(400));
+    collection.insertOne({'user': user, 'passwd': passwd}, function(err, result) {
+        if (err)
+            return next(new HttpError(400, "User already exists"));
+            // TODO: change error code
+        var token = "123";
+        res.json({token: token});
+    });
 });
 
-router.post('/signin', function(req, res) {
-    res.send('hello');
+router.post('/signin', function(req, res, next) {
+    var collection = db.collection("users");
+    var user = req.body.user || "";
+    var passwd = req.body.password || "";
+    
+    if (!user || !passwd)
+        return next(new HttpError(400));
+    
+    collection.findOne({'user': user}, function(err, item) {
+        if (err || !item)
+            return next(new HttpError(404));
+        if (item.passwd != passwd)
+            return next(new HttpError(404));
+        
+        var token = "777";
+        res.json({token: token});
+    });
 });
 
 router.get('/games', function(req, res, next) {
